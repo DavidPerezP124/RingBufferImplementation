25b27a820bede53d990f0ae64788b2ea5226a5a0
David Perez <davidpp124@gmail.com>
2020-11-07 21:09:50 -0600

    Initial Commit

--- RingBuffer/main.swift
+++ RingBuffer/main.swift
@@ -7,5 +7,133 @@
 
 import Foundation
 
-print("Hello, World!")
+func example(of example: String, block: () -> Void){
+    print("-- Example of \(example) --")
+    block()
+}
 
+public protocol Queue {
+    associatedtype Element
+    mutating func enqueue(_ element:Element) -> Bool
+    mutating func dequeue() -> Element?
+    var isEmpty: Bool { get }
+    var peek: Element? { get }
+}
+
+public struct RingBuffer<T>{
+    fileprivate var array: [T?]
+    fileprivate var readIndex = 0
+    fileprivate var writeIndex = 0
+    
+    public init(count: Int){
+        array = [T?](repeating: nil, count: count)
+    }
+    @discardableResult
+    public mutating func write(_ element: T?) -> Bool{
+        if !isFull{
+            array[writeIndex % array.count] = element
+            writeIndex += 1
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    public mutating func read() -> T? {
+        if !isEmpty{
+            let element = array[readIndex % array.count]
+            readIndex += 1
+            return element
+        } else {
+            return nil
+        }
+    }
+
+    
+    fileprivate var availableSpaceForReading: Int{
+        return writeIndex - readIndex
+    }
+    
+    fileprivate var availableSpaceForWriting: Int{
+        return array.count - availableSpaceForReading
+    }
+    
+    public var first: T? {
+        guard let first = array.first else {return nil}
+        return first
+    }
+    
+    public var isEmpty: Bool {
+        return availableSpaceForReading == 0
+    }
+    
+    public var isFull: Bool{
+        return availableSpaceForWriting ==  0
+    }
+}
+
+extension RingBuffer : CustomStringConvertible {
+    public var description: String {
+        return (array.reduce(into: "[ ") { $0 += "\(String(describing: $1)), "} + "]").replacingOccurrences(of: "Optional", with: "").replacingOccurrences(of: "(", with: "").replacingOccurrences(of: ")", with: "").replacingOccurrences(of: ", ]", with: " ]")
+    }
+}
+
+
+public struct QueueRingBuffer<T>: Queue {
+    private var ringBuffer: RingBuffer<T>
+    
+    @discardableResult
+    public mutating func enqueue(_ element: T) -> Bool {
+        return ringBuffer.write(element)
+    }
+    
+    public mutating func dequeue() -> T? {
+        return isEmpty ? nil : ringBuffer.read()
+    }
+    
+    public var isEmpty: Bool {
+        return ringBuffer.isEmpty
+    }
+    
+    public var peek: T? {
+        return ringBuffer.first
+    }
+    
+    public typealias Element = T
+ 
+    
+    public init(count:Int){
+        ringBuffer = RingBuffer<T>(count:count)
+    }
+    
+    
+}
+
+extension QueueRingBuffer: CustomStringConvertible {
+    public var description: String {
+        return ringBuffer.description
+    }
+}
+
+
+example(of: "Implementing a Ring Buffer") {
+    var queue = QueueRingBuffer<Int>(count: 10)
+    queue.enqueue(2)
+    print("Peek",queue.peek!)
+    queue.enqueue(5)
+    queue.enqueue(10)
+    print("Dequeue",queue.dequeue()!)
+    print("Dequeue",queue.dequeue()!)
+    queue.enqueue(10)
+    queue.enqueue(15)
+    queue.enqueue(10)
+    queue.enqueue(15)
+    queue.enqueue(10)
+    print("Peek",queue.peek!)
+    queue.enqueue(15)
+    print("Dequeue",queue.dequeue()!)
+    print("Dequeue",queue.dequeue()!)
+    print("Dequeue",queue.dequeue()!)
+    print("Peek",queue.peek!)
+    print(queue.description)
+}
